e07d0595d2a181384d9bd7cbf51f8ec7308a7788
BlackSwam666 <lebed@mail.ru>
2023-07-30 22:09:45 +0300

    Это расписание для учителей

--- zadanie.rb
+++ zadanie.rb
@@ -1,1 +1,23 @@
-p "hello"
+@days = ["mon", "tue", "wen", "thu", "fri", "sun", "sat"] * 53 
+ 
+def get_months(number, month) 
+ month = {} 
+ (1..number).each do |i| 
+ month[i] = @days[i-1] 
+ end 
+ (1..number).each do |i| 
+ @days.delete_at(0) 
+ end 
+ return month 
+end 
+count = -1 
+@months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"] 
+@months.each do |i| 
+ if (count += 1) % 2 == 0 
+ p get_months(31, i) 
+ elsif i == "feb" 
+ p get_months(28, i) 
+ else 
+ p get_months(30, i) 
+ end 
+end
