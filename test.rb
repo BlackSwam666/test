1c58aea964fe68854a289c610380ec1a6512b7eb
BlackSwam666 <lebed@mail.ru>
2023-07-31 21:13:02 +0300

    Это алгоритм чисел

--- test.rb
+++ test.rb
@@ -1,1 +1,14 @@
-p "HELLO"
+p "Enter the chislo:"
+k = gets.to_i
+if 1 <= k && k <= 10^6
+   for i in (1..10) do
+      if (k % 2 == 0) 
+         k = k / 2
+      else
+         k = (k* 3) + 1
+      end
+      p k
+   end
+else
+   p "Uncorrected chislo"
+end
