e188d107285cf095daaa668055b1ab6be3a895e0
BlackSwam666 <lebed@mail.ru>
2023-08-02 23:52:54 +0300

    Это попытка вывода экрана загрузки

--- dz2.rb
+++ dz2.rb
@@ -3,6 +3,6 @@ d = "o"
 b = "o"
 require "colorize"
 m.each do |i|
-   puts (d * i).green + (b * (10 - i)).red
+    print "\r%-5s" % (((d * i).green + (b * (10 - i)).red)).overlined
     sleep(5)
 end
